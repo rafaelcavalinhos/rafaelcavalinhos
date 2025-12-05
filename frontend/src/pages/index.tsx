d7bb8b3f5c58d68605167fd18e11fa5001c87974
rafa.cavalinhos <rafaelcavalinhos2002@gmail.com>
2025-12-05 14:37:48 +0000

    center cat

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <main>
+    <div className="flex flex-row items-center justify-center bg-white h-full w-full">
         <Image src="/cat.gif" alt="cat" width={1000} height={1000} unoptimized/>
-      </main>
     </div>
   );
 }
