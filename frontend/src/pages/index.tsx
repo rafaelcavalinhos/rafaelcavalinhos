557b6094b5deafcea1c955f08b1f2c5212067ffa
rafa.cavalinhos <rafaelcavalinhos2002@gmail.com>
2025-12-09 21:27:57 +0000

    update

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { FlexCenter, FlexCol, FlexRow } from "@/components/utils/flex";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload, faEnvelope, faMailBulk, faMailForward, faMailReply, faVoicemail } from "@fortawesome/free-solid-svg-icons";
-import { faGit, faGithub, faInstagram, faLetterboxd, faLinkedin, faMailchimp } from "@fortawesome/free-brands-svg-icons";
+import { faArrowRight, faCalendar, faDownload, faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
 
@@ -11,22 +11,25 @@ export default function Home() {
 
   const softSkills = ["Problem Solving", ];
 
-  const hobbies = ["Playing Guitar", "Gaming", "Programming", ];
+  const hobbies = ["Playing Guitar", "Gaming", "Programming", "Petting my cat", "Cars"];
 
   return (<>
+
+    {/* Under Construction */}
     <FlexRow className="justify-center z-50 fixed top-20 w-full pointer-events-none">
       <div className="pointer-events-auto px-6 py-2 bg-yellow-300 text-black font-semibold rounded-2xl shadow-md border-2 border-yellow-100">
         🚧 Under Construction 🚧  
       </div>
     </FlexRow>
 
-    <div id="home" className=" bg-white">
+    <div id="home" className="hidden lg:block bg-white">
 
-      <header className="fixed flex flex-row justify-between items-center w-full py-4 px-10 border-b-2 border-gray-200 bg-white">
+      {/* Header */}
+      <header className="z-50 fixed flex flex-row justify-between items-center w-full py-4 px-10 border-b-2 border-gray-200 bg-white">
         <h1 className="font-semibold text-2xl text-scale"><a href="#home">Rafael Cavalinhos</a></h1>
         <FlexRow className="font-semibold text-xl gap-x-4">
           <a href="#about" className="text-scale">About</a>
-          <a href="#about" className="text-scale">Education</a>
+          <a href="#education" className="text-scale">Education</a>
           <a href="#projects" className="text-scale">Projects</a>
           <a href="#contact" className="text-scale">Contact</a>
         </FlexRow>
@@ -34,13 +37,12 @@ export default function Home() {
 
       <main>
 
-        <div className="flex flex-col">
+        <FlexCol className="w-full">
 
+          {/* Hero Section */}
           <FlexRow className="justify-between h-screen pt-24 px-20">
             <FlexCol className="font-bold text-6xl justify-center items-start gap-y-10">
-              <h1>Hello!</h1>
-              <h1>I'm <span className="text-gradient">Rafael Cavalinhos</span>,</h1>
-              <h1>a Full-Stack Developer</h1>
+              <h1>Hello!<br />I'm <span className="text-gradient">Rafael Cavalinhos</span>,<br />a Full-Stack Developer</h1>
               <button onClick={() => window.open("/cv.pdf", "_blank")} className="cursor-pointer group bg-blue-400 border-3 border-blue-200 text-white rounded-2xl text-xl py-2 px-10 transition duration-200 hover:scale-120">
                 <span className="inline-flex items-center gap-2 transition duration-300 group-hover:scale-125">
                   Download CV <FontAwesomeIcon icon={faDownload} />
@@ -63,11 +65,18 @@ export default function Home() {
               </FlexRow>
 
             </FlexCol>
-            <Image src="/cat.gif" alt="cat" width={700} height={600}/>
+
+            <FlexCenter className="pr-20">
+              <Image src="/cat.gif" alt="me" width={500} height={500} className="rounded-full shadow-2xl hover:scale-105 transition"/>
+              <p className="text-6xl absolute text-yellow-500 text-center">Image not<br />available yet...</p>
+            </FlexCenter>
           </FlexRow>
 
-          <FlexCol id="about" className="items-center gap-y-4 mt-50 bg-gray-50 w-full py-20">
+          {/* About Section*/}
+          <FlexCol id="about" className="items-center gap-y-4 mt-50 bg-gray-50 w-full py-20 px-20">
             <h1 className="title">About</h1>
+
+            <FlexRow className="">
             
               <FlexCol className="gap-y-4 w-[50%] items-center text-lg font-medium">
                 <h2 className="text-3xl text-gradient">Professional Bio</h2>
@@ -80,10 +89,10 @@ export default function Home() {
                 </FlexCol>
               </FlexCol>
             
-              <FlexCol className="gap-y-4 w-[40%] items-center text-lg font-medium">
+              <FlexCol className="gap-y-4 w-[50%] items-center text-lg font-medium">
 
-                <h2 style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-3xl text-gradient mt-10">Tech Interests</h2>
-                <FlexRow className="flex-wrap gap-y-2 gap-x-1">
+                <h2 style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-3xl text-gradient">Tech Interests</h2>
+                <FlexRow className="flex-wrap gap-y-2 gap-x-1 justify-center">
                   {techInterests.map((interest, i) => {
                     return (
                       <div key={i} className="group hover:scale-125 transition duration-300 border-2 rounded-3xl p-1 px-3 border-blue-200 bg-blue-400 text-white">
@@ -93,8 +102,8 @@ export default function Home() {
                   })}
                 </FlexRow>
 
-                <h2 style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-3xl text-gradient mt-10">Soft Skills</h2>
-                <FlexRow className="flex-wrap gap-y-2 gap-x-1">
+                <h2 style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-3xl text-gradient mt-5">Soft Skills</h2>
+                <FlexRow className="flex-wrap gap-y-2 gap-x-1 justify-center">
                   {softSkills.map((skill, i) => {
                     return (
                       <div key={i} className="group hover:scale-125 transition duration-300 border-2 rounded-3xl p-1 px-3 border-blue-200 bg-blue-400 text-white">
@@ -104,8 +113,8 @@ export default function Home() {
                   })}
                 </FlexRow>
 
-                <h2 style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-3xl text-gradient mt-10">Hobbies</h2>
-                <FlexRow className="flex-wrap gap-y-2 gap-x-1">
+                <h2 style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-3xl text-gradient mt-5">Hobbies</h2>
+                <FlexRow className="flex-wrap gap-y-2 gap-x-1 justify-center">
                   {hobbies.map((hobby, i) => {
                     return (
                       <div key={i} className="group hover:scale-125 transition duration-300 border-2 rounded-3xl p-1 px-3 border-blue-200 bg-blue-400 text-white">
@@ -117,12 +126,80 @@ export default function Home() {
 
               </FlexCol>
 
-
+            </FlexRow>
 
           </FlexCol>
 
-          <FlexCol id="projects" className="items-center gap-y-4 py-20">
+          {/* Education Section */}
+          <FlexCol id="education" className="items-center gap-y-4 py-20 px-20">
+            <h1 className="title">Education</h1>
+
+            <FlexCol className="w-full gap-y-4">
+
+              <FlexCol className="group w-[70%] justify-start border-4 rounded-2xl px-5 py-3 bg-blue-400 border-blue-200 text-white hover:scale-105 transition duration-300">
+                <span className="group-hover:scale-102 transition">
+                  <p style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-xl font-bold text-gradient-waves">Licenciatura em Engenharia Informática</p>
+                  <p className="text-base">Instituto Politécnico de Setúbal</p>
+                  <FlexRow className="gap-x-4 items-center mt-2 mb-4 font-semibold">
+                    <p><FontAwesomeIcon icon={faCalendar}></FontAwesomeIcon> 10/2023 - 10/2025</p>
+                    <p><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon> Setúbal - Portugal</p>
+                  </FlexRow>
+
+                  <p>
+                    Bachelor’s degree focused on developing technical and scientific skills in the field of computer science.
+                    It covers programming, databases, networks, software development, and technology integration.
+                    The program combines theory and practice through laboratory projects and teamwork, preparing professionals for the design, analysis,
+                    and implementation of modern IT solutions.
+                  </p>
+                </span>
+              </FlexCol>
+
+              <FlexCol className="group w-[70%] justify-start border-4 rounded-2xl px-5 py-3 bg-blue-400 border-blue-200 text-white hover:scale-105 transition duration-300">
+                <span className="group-hover:scale-102 transition">
+                  <p style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-xl font-bold text-gradient-waves">Desenvolvimento de Videojogos e Aplicações Multimédia</p>
+                  <p className="text-base">Instituto Politécnico de Setúbal</p>
+                  <FlexRow className="gap-x-4 items-center mt-2 mb-4 font-semibold">
+                    <p><FontAwesomeIcon icon={faCalendar}></FontAwesomeIcon> 10/2020 - 06/2023</p>
+                    <p><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon> Setúbal - Portugal</p>
+                  </FlexRow>
+
+                  <p>
+                    Programme focused on game development and multimedia applications, covering game design techniques, interface design and core programming concepts.
+                    It includes work with graphic APIs and game engines, as well as the creation and editing of multimedia assets such as audio, video, textures and 3D models.
+                    The curriculum also explores serious games, gamification, virtual and augmented reality, and technologies for cross-platform development on PC, Web,
+                    mobile and console. This provides a practical and well-rounded foundation for building modern interactive digital experiences.
+                  </p>
+                </span>
+              </FlexCol>
+
+              <FlexCol className="group w-[70%] justify-start border-4 rounded-2xl px-5 py-3 bg-blue-400 border-blue-200 text-white hover:scale-105 transition duration-300">
+                <span className="group-hover:scale-102 transition">
+                  <p style={{ animationDelay: `${Math.random() * 0.5}s` }} className="text-xl font-bold text-gradient-waves">Técnico de Informática de Gestão</p>
+                  <p className="text-base">Escola Tecnológica do Litoral Alentejano</p>
+                  <FlexRow className="gap-x-4 items-center mt-2 mb-4 font-semibold">
+                    <p><FontAwesomeIcon icon={faCalendar}></FontAwesomeIcon> 09/2017 - 07/2020</p>
+                    <p><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon> Sines - Portugal</p>
+                  </FlexRow>
+
+                  <p>
+                    Focused on the development and maintenance of IT solutions for business environments.
+                    Experience includes creating websites and company portals, installing and configuring operating systems and application software,
+                    and managing relational databases. Responsibilities also covered developing automated data-processing routines using programming or
+                    scripting languages, collaborating in system analysis and development teams, and building management-oriented software applications.
+                    Additional competencies include handling administrative office tasks and working with business software for invoicing, inventory, accounting,
+                    payroll and financial management, as well as proficient use of word processing tools, spreadsheets and other productivity applications.
+                  </p>
+                </span>
+              </FlexCol>
+
+            </FlexCol>
+    
+          </FlexCol>
+
+          <FlexCol id="projects" className="items-center gap-y-4 mt-50 bg-gray-50 w-full py-20 px-20">
             <h1 className="title">Projects</h1>
+
+            <p className="text-center text-6xl text-gradient py-6">Work in progress...</p>
     
           </FlexCol>
 
@@ -131,14 +208,14 @@ export default function Home() {
     
           </FlexCol>
 
-        </div>
+        </FlexCol>
 
       </main>
 
     </div>
 
-    {/* <FlexCenter className="block lg:hidden h-full font-bold text-2xl">
+    <FlexCenter className="block lg:hidden h-full font-bold text-2xl">
       <p className="text-gradient text-center">Please open this webpage on a computer.</p>
-    </FlexCenter> */}
+    </FlexCenter>
   </>);
 }
