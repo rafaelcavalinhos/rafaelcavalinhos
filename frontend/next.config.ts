0ad377fb851a73cabce9e9ce39c26e311a87554a
rafa.cavalinhos <rafaelcavalinhos2002@gmail.com>
2026-04-23 21:21:26 +0100

    update page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'pt'],
+    defaultLocale: 'en',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '2wcolulh7c.ufs.sh',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,17 @@
-import "@/styles/globals.css";
-import "@/styles/animations.css";
-import type { AppProps } from "next/app";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import '@/styles/globals.css';
+import '@/styles/animations.css';
+import type { AppProps } from 'next/app';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { IntlProvider } from 'next-intl';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <IntlProvider locale={pageProps.locale} messages={pageProps.messages}>
+        <Component {...pageProps} />
+      </IntlProvider>
+    </>
+  );
 }
